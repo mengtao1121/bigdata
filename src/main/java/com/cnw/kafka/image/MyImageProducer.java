ae42e585aa8c99f1793c5092df8c2232f824a253
mengtao1121 <13541385923@163.com>
2019-05-11 22:26:14 +0800

    fix send iamge

--- src/main/java/com/cnw/kafka/image/MyImageProducer.java
+++ src/main/java/com/cnw/kafka/image/MyImageProducer.java
@@ -6,7 +6,6 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import util.MyCallback;
-import util.Person;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,7 +21,7 @@ public class MyImageProducer {
         kafkaprops.put("value.serializer", ByteArraySerializer.class.getName());
 
         //创建kafka生产者
-        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(kafkaprops);
+        KafkaProducer<String, byte[]> kafkaProducer = new KafkaProducer<String, byte[]>(kafkaprops);
         try {
             //使用Java IO流读取取文件
             File file = new File("kafka_logo.png");
@@ -33,7 +32,7 @@ public class MyImageProducer {
             inputStream.read(buffer);
             //发送消息的key为文件名称，value为buffer数组（文件内容）
             ProducerRecord<String, byte[]> record = new ProducerRecord<String, byte[]>("myFirstTopic", file.getName(), buffer);
-            kafkaProducer.send(record,new MyCallback());
+            kafkaProducer.send(record);
             kafkaProducer.flush();
         } catch (IOException e) {
             e.printStackTrace();
